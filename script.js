7d07d3b9af1d58a63d0b7847cf4258af4b1c0607
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-06-01 18:37:36 +0600

    recap js lightly

--- script.js
+++ script.js
@@ -10,7 +10,44 @@
 // }
 // abc();
 
-let a = [12, 54, 8, 76, 536, 146, 497, 64, 68, 32];
-a.forEach((value) => {
-  console.log(value - 10);
-});
+// let a = [12, 54, 8, 76, 536, 146, 497, 64, 68, 32];
+// a.forEach((value) => {
+//   console.log(value - 10);
+// });
+
+// console.log(a, 'default array');
+
+// let person = {
+//   name: "John",
+//   age: 26,
+//   city: "NY",
+// };
+
+// for (let key in person) {
+//   console.log(key, ":", person[key]);
+// }
+
+// let a = 12;
+// do {
+//     console.log('hey');
+//     a++;
+// }
+// while (a < 15)
+
+// () => {
+
+// }
+
+// setTimeout(() => {
+//   console.log("2 second por aso");
+// }, 2000);
+
+
+
+// const a = ()=> {
+
+// }
+// function abc(a) {
+// a()
+// }
+// abc(function () {console.log('hey');})
