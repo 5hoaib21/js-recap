8d2bc25c3842ae8d9d43b3cd1a371203d2c61686
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-06-01 17:41:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// var~ let~ const~
+// const vaR = 'don`t use it';
+// console.log(vaR);
+
+// function abc() {
+//     for(let i = 1; i< 12; i++){
+//         console.log('first',i);
+//     }
+//     console.log('second',i);
+// }
+// abc();
+
+let a = [12, 54, 8, 76, 536, 146, 497, 64, 68, 32];
+a.forEach((value) => {
+  console.log(value - 10);
+});
